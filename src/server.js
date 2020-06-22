7ef791b1968c9827e42f5cc241c4853fb0807b23
pedro-limma <pedro.lima.wk@gmail.com>
2020-06-22 00:39:12 -0300

    uma parte do código de conexão com o Arduino

--- src/server.js
+++ src/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const http = require('http');
+const PORT = 3333;
 const routes = require('./routes');
-const cors = require('cors')
+const cors = require('cors');
+const socketIO = require('socket.io');
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () =>{
+    console.log('User disconnected')
+  })
+})
 
 app.use(cors())
 app.use(express.json());
